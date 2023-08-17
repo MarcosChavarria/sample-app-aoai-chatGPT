766ca7c11524a2a3aaff76012f8688a25e29c3a7
MarcosChavarria <marcoschjcr@gmail.com>
2023-08-17 15:40:34 -0600

    Quita la seguridad

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -33,13 +33,14 @@ const Chat = () => {
     const [showAuthMessage, setShowAuthMessage] = useState<boolean>(false);
     
     const getUserInfoList = async () => {
-        const userInfoList = await getUserInfo();
-        if (userInfoList.length === 0 && window.location.hostname !== "127.0.0.1") {
-            setShowAuthMessage(false);
-        }
-        else {
-            setShowAuthMessage(false);
-        }
+        //const userInfoList = await getUserInfo();
+        //if (userInfoList.length === 0 && window.location.hostname !== "127.0.0.1") {
+        //    setShowAuthMessage(true);
+        //}
+        //else {
+        //    setShowAuthMessage(false);
+        //}
+        setShowAuthMessage(false);
     }
 
     const makeApiRequest = async (question: string) => {
